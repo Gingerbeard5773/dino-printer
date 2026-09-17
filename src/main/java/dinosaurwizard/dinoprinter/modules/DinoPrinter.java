5e44a6299a470a03ef703e30999241ebabe88d8a
Gingerbeard <fathergingerbeard@gmail.com>
2026-09-17 03:19:33 -0400

    mc 1.21.10
    Backwards compatible with 1.21.9

--- src/main/java/dinosaurwizard/dinoprinter/modules/DinoPrinter.java
+++ src/main/java/dinosaurwizard/dinoprinter/modules/DinoPrinter.java
@@ -7,6 +7,7 @@
 package dinosaurwizard.dinoprinter.modules;
 
 import dinosaurwizard.dinoprinter.utils.BlockPrint;
+import fi.dy.masa.litematica.config.Configs;
 import fi.dy.masa.litematica.world.SchematicWorldHandler;
 import fi.dy.masa.litematica.world.WorldSchematic;
 import meteordevelopment.meteorclient.MeteorClient;
@@ -369,6 +370,10 @@ public class DinoPrinter extends Module {
             slot.lockTimer = Math.min(slotLockTicks.get(), slot.lockTimer + 1);
             slot.syncTimer = Math.min(antiOverrideTicks.get(), slot.syncTimer + 1);
         }
+
+        // Rendering must be active
+        if (!Configs.Visuals.ENABLE_RENDERING.getBooleanValue()) return;
+        if (!Configs.Visuals.ENABLE_SCHEMATIC_RENDERING.getBooleanValue()) return;
 
         if (shouldPause()) return;
 
